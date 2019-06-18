f3cf99ad22279b5d5a8803daf805dcd01e90e2f3
Владимир Ливинский <xDamneDx@Gmail.com>
2019-06-18 15:22:15 +0300

    Добавляет сценарии взаимодействия с сайтом и изменение персонажа при нажатии.

--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,87 @@
 'use strict';
 
+var getRndNum = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var KEYCODE = {
+  enter: 13,
+  esc: 27
+};
+var WIZARD_COLORS = {
+  coat: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
+  eyes: ['black', 'red', 'blue', 'yellow', 'green'],
+  fireball: ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848']
+};
 var userSetup = document.querySelector('.setup');
-userSetup.classList.remove('hidden');
+var userSetupOpen = document.querySelector('.setup-open');
+var userSetupClose = userSetup.querySelector('.setup-close');
+var userSetupOpenIcon = userSetupOpen.querySelector('.setup-open-icon');
+var userSetupNameInput = userSetup.querySelector('.setup-user-name');
+var userSetupCharacter = document.querySelector('.setup-wizard');
+var userCharacterCoat = userSetupCharacter.querySelector('.wizard-coat');
+var setupPlayer = document.querySelector('.setup-player');
+var userCharacterCoatInput = setupPlayer.querySelector('input[name="coat-color"]');
+var userCharacterEyes = userSetupCharacter.querySelector('.wizard-eyes');
+var userCharacterEyesInput = setupPlayer.querySelector('input[name="eyes-color"]');
+var userCharacterFireball = userSetup.querySelector('.setup-fireball-wrap');
+var userCharacterFireballInput = userCharacterFireball.querySelector('input[name="fireball-color"]');
+
 userSetup.querySelector('.setup-similar').classList.remove('hidden');
+
+var setupEscPressHandler = function (evt) {
+  if (evt.keyCode === KEYCODE.esc && document.activeElement !== userSetupNameInput) {
+    closeSetup();
+  }
+};
+
+var openSetup = function () {
+  userSetup.classList.remove('hidden');
+  document.addEventListener('keydown', setupEscPressHandler);
+};
+
+var closeSetup = function () {
+  userSetup.classList.add('hidden');
+  document.removeEventListener('keydown', setupEscPressHandler);
+};
+
+userSetupOpen.addEventListener('click', openSetup);
+userSetupClose.addEventListener('click', closeSetup);
+
+userSetupOpenIcon.addEventListener('focus', function () {
+  userSetupOpenIcon.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === KEYCODE.enter) {
+      openSetup();
+    }
+  });
+});
+
+userSetupClose.addEventListener('focus', function () {
+  userSetupClose.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === KEYCODE.enter) {
+      closeSetup();
+    }
+  });
+});
+
+var getRndColor = function (typeOfPart) {
+  return typeOfPart[getRndNum(0, typeOfPart.length - 1)];
+};
+
+userCharacterCoat.addEventListener('click', function () {
+  userCharacterCoat.style.fill = getRndColor(WIZARD_COLORS.coat);
+  userCharacterCoatInput.value = userCharacterCoat.style.fill;
+});
+
+userCharacterEyes.addEventListener('click', function () {
+  userCharacterEyes.style.fill = getRndColor(WIZARD_COLORS.eyes);
+  userCharacterEyesInput.value = userCharacterEyes.style.fill;
+});
+
+userCharacterFireball.addEventListener('click', function () {
+  userCharacterFireball.style.background = getRndColor(WIZARD_COLORS.fireball);
+  userCharacterFireballInput.value = userCharacterFireball.style.background;
+});
 
 var NUM_OF_WIZARDS = 4;
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
@@ -12,9 +91,6 @@ var fragment = document.createDocumentFragment();
 var getCharactersData = function (numOfCharacters) {
   var charactersData = [];
   var getRndData = function () {
-    var getRndNum = function (min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
     var firstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
     var secondNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
     var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
